4116d794a3f259c66ae3a2f6f0b5ff2457b44919
school <yho45@my.centennialcollege.ca>
2025-05-07 22:59:10 -0400

    credit update

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -3,7 +3,7 @@
 // This code is original and owned by yobisaboy. 
 // Use requires logo inclusion and credit in-game and on publishing platforms.
 // Redistribution or modification must include proper attribution.
-// Contact: yobisaboy@gmail.com | +1 (647) 637-0831
+// Contact: yobisaboy@gmail.com
 // -----------------------------------------------------------------------------
 
 using System.Collections.Generic;
