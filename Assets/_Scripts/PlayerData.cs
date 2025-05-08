fa62f3bec512db5e8b21096b1494b2b42902a18f
school <yho45@my.centennialcollege.ca>
2025-05-07 22:47:50 -0400

    credits

--- Assets/_Scripts/PlayerData.cs
+++ Assets/_Scripts/PlayerData.cs
@@ -1,5 +1,12 @@
+// -----------------------------------------------------------------------------
+// Created by: yobisaboy
+// This code is original and owned by yobisaboy. 
+// Use requires logo inclusion and credit in-game and on publishing platforms.
+// Redistribution or modification must include proper attribution.
+// Contact: yobisaboy@gmail.com | +1 (647) 637-0831
+// -----------------------------------------------------------------------------
+
 using System.Collections.Generic;
-using UnityEngine;
 
 [System.Serializable]
 public class PlayerData
---
+++ README.md
@@ -0,0 +1,9 @@
+<img src="https://github.com/yobisaboy/Resume/blob/main/yobisaboyLogo.png" alt="yobisaboy Logo" height="90" />
+
+**This code is original and owned by yobisaboy.**  
+Use requires logo inclusion and credit in-game and on publishing platforms.  
+Redistribution or modification must include proper attribution.
+
+---
+
+[yobisaboy@gmail.com](mailto:yobisaboy@gmail.com) | +1 (647) 637-0831
