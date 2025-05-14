ef2d20cdec5dc3cf53797c427ab79fd628ce1dd1
school <yho45@my.centennialcollege.ca>
2025-05-13 22:50:57 -0400

    marker

--- Assets/_Scripts/UI/ButtonImageColorChange.cs
+++ Assets/_Scripts/UI/ButtonImageColorChange.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class ButtonImageColorChange : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public Color pressedFrameColor;
     public Color pressedImageColor;
+    public float rotationSpeed = 300f;
 
     private Image _frame;
     private Image _image;
+    private GameObject _ring;
     private Color _originalFrameColor;
     private Color _originalImageColor;
+    private bool _isPressed = false;
 
     void Start()
     {
         _frame = transform.Find("Frame").GetComponent<Image>();
         _image = transform.Find("Image").GetComponent<Image>();
+        if(transform.Find("Ring") != null)
+        {
+            _ring = transform.Find("Ring").gameObject;
+        }
+            
         if (_frame != null)
         {
             _originalFrameColor = _frame.color;
@@ -26,8 +35,25 @@ public class ButtonImageColorChange : MonoBehaviour, IPointerDownHandler, IPoint
         }
     }
 
+    void Update()
+    {
+        if (_ring != null)
+        {
+            if (_isPressed)
+            {
+                _ring.GetComponent<RectTransform>().Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+            }
+
+            _ring.GetComponent<Image>().enabled = _isPressed;
+        }
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (_ring != null)
+        {
+            _isPressed = true;
+        }
         if (_frame != null)
         {
             _frame.color = pressedFrameColor;
@@ -35,11 +61,16 @@ public class ButtonImageColorChange : MonoBehaviour, IPointerDownHandler, IPoint
         if (_image != null)
         {
             _image.color = pressedImageColor;
-        }     
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        if (_ring != null)
+        {
+            _isPressed = false;
+            _ring.GetComponent<RectTransform>().eulerAngles = Vector3.zero;
+        }
         if (_frame != null)
         {
             _frame.color = _originalFrameColor;
